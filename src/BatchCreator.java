f6e6cccddcb06e7385e98ade98c80f31fd4aa618
Kağan Baldıran <80681941+KaganBaldiran@users.noreply.github.com>
2023-06-04 15:02:20 +0200

    Add files via upload

--- src/BatchCreator.java
+++ src/BatchCreator.java
@@ -6,34 +6,44 @@ public class BatchCreator
 {
     public static void Create()
     {
-        String batchScript = "@echo off\n" +
-                "\n" +
-                "REM Detect the operating system\n" +
-                "if \"%PROCESSOR_ARCHITECTURE%\"==\"AMD64\" (\n" +
-                "    set OS_ARCH=64\n" +
-                ") else (\n" +
-                "    set OS_ARCH=32\n" +
-                ")\n" +
-                "\n" +
-                "REM Install the Java SDK\n" +
-                "if \"%OS_ARCH%\"==\"64\" (\n" +
-                "    REM Install the Java SDK for Windows 64-bit\n" +
-                "    echo Installing Java SDK 64-bit...\n" +
-                "    start /wait \"\" \"exes/jdk-20.0.1_windows-x64_bin.exe\"\n" +
-                ") else (\n" +
-                "    REM Install the Java SDK for macOS 64-bit\n" +
-                "    echo Installing Java SDK 64-bit...\n" +
-                "    open -W \"exes/jdk-20.0.1_macos-x64_bin.dmg\"\n" +
-                ")\n" +
-                "\n" +
-                "REM Verify the Java installation\n" +
-                "java -version";
+        String batchFileContent = "@echo off\n"
+                + "\n"
+                + "REM Detect the operating system\n"
+                + "if \"%PROCESSOR_ARCHITECTURE%\"==\"AMD64\" (\n"
+                + "    set OS_ARCH=64\n"
+                + ") else (\n"
+                + "    set OS_ARCH=32\n"
+                + ")\n"
+                + "\n"
+                + "REM Install the Java SDK\n"
+                + "if \"%OS_ARCH%\"==\"64\" (\n"
+                + "    set \"downloadUrl=https://download.oracle.com/java/20/archive/jdk-20.0.1_windows-x64_bin.exe\"\n"
+                + "    set \"saveLocation=%~dp0jdk-20.0.1_windows-x64_bin.exe\"\n"
+                + ") else (\n"
+                + "    set \"downloadUrl=https://download.oracle.com/java/20/archive/jdk-20.0.1_macos-x64_bin.dmg\"\n"
+                + "    set \"saveLocation=%~dp0jdk-20.0.1_macos-x64_bin.dmg\"\n"
+                + ")\n"
+                + "\n"
+                + "echo Downloading the SDK...\n"
+                + "\n"
+                + "REM Download the file using curl or bitsadmin\n"
+                + "bitsadmin /transfer DownloadJavaSDK20.0.1 /download /priority high \"%downloadUrl%\" \"%saveLocation%\"\n"
+                + "REM Check if the file exists\n"
+                + ":check_file\n"
+                + "if exist \"%saveLocation%\" (\n"
+                + "    echo Download completed.\n"
+                + "    echo Installing the SDK...\n"
+                + "    start /wait \"\" \"%saveLocation%\"\n"
+                + ") else (\n"
+                + "    goto check_file\n"
+                + ")\n"
+                + "\n"
+                + "REM Rest of your batch file";
 
-        
         String batchFilePath = "InstallSDK.bat";
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(batchFilePath))) {
-            writer.write(batchScript);
+            writer.write(batchFileContent);
         } catch (IOException e) {
             e.printStackTrace();
         }
